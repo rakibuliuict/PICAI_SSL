cfee3ba217542ca18402cc7b39b4818dd191332f
Rakibul <rakibuliuict@gmail.com>
2025-03-22 23:43:13 +0600

    picaiDataset

--- Basecode/dataloaders/picaiDataset.py
+++ Basecode/dataloaders/picaiDataset.py
@@ -9,10 +9,10 @@ import logging
 
 
 class PICAIDataset(Dataset):
-    """ PICAI Dataset """
+    """ PICAI Dataset with multi-modal MRI and segmentation mask """
 
     def __init__(self, data_dir, list_dir, split, reverse=False, logging=logging):
-        self.data_dir = data_dir  # No subfolder like 'Training Set'
+        self.data_dir = data_dir
         self.list_dir = list_dir
         self.split = split
         self.reverse = reverse
@@ -40,7 +40,6 @@ class PICAIDataset(Dataset):
         with open(data_path, 'r') as f:
             self.image_list = f.read().splitlines()
 
-        # Now files are named like: /path/to/data/10005/10005.h5
         self.image_list = [os.path.join(self.data_dir, pid, f"{pid}.h5") for pid in self.image_list]
 
         logging.info("{} set: total {} samples".format(split, len(self.image_list)))
@@ -54,14 +53,22 @@ class PICAIDataset(Dataset):
         if self.reverse:
             image_path = self.image_list[len(self.image_list) - idx % len(self.image_list) - 1]
 
-        h5f = h5py.File(image_path, 'r')
-        image, label = h5f['image'][:], h5f['label'][:].astype(np.float32)
-        samples = image, label
+        with h5py.File(image_path, 'r') as h5f:
+            t2w = h5f['image']['t2w'][:]
+            adc = h5f['image']['adc'][:]
+            hbv = h5f['image']['hbv'][:]
+            seg = h5f['label']['seg'][:].astype(np.float32)
+
+        image = np.stack([t2w, adc, hbv], axis=0)  # Shape: [3, H, W, D]
+        samples = image, seg
 
         if self.transform:
-            tr_samples = self.transform(samples)
-        image_, label_ = tr_samples
+            image_, label_ = self.transform(samples)
+        else:
+            image_, label_ = image, seg
+
         return image_.float(), label_.long()
+
 
 class CenterCrop(object):
     def __init__(self, output_size):
@@ -89,107 +96,61 @@ class CenterCrop(object):
         return do_transform
 
     def __call__(self, samples):
-        transform = self._get_transform(samples[0])
-        return [transform(s) for s in samples]
+        transform = self._get_transform(samples[1])  # Use label to compute crop
+        return [transform(samples[0]), transform(samples[1])]
 
 
 class RandomCrop(object):
-    """
-    Crop randomly the image in a sample
-    Args:
-    output_size (int): Desired output size
-    """
-
     def __init__(self, output_size, with_sdf=False):
         self.output_size = output_size
         self.with_sdf = with_sdf
 
     def _get_transform(self, x):
-        if x.shape[0] <= self.output_size[0] or x.shape[1] <= self.output_size[1] or x.shape[2] <= self.output_size[2]:
-            pw = max((self.output_size[0] - x.shape[0]) // 2 + 1, 0)
-            ph = max((self.output_size[1] - x.shape[1]) // 2 + 1, 0)
-            pd = max((self.output_size[2] - x.shape[2]) // 2 + 1, 0)
-            x = np.pad(x, [(pw, pw), (ph, ph), (pd, pd)], mode='constant', constant_values=0)
+        if x.shape[1] <= self.output_size[1] or x.shape[2] <= self.output_size[2] or x.shape[3] <= self.output_size[2]:
+            pw = max((self.output_size[1] - x.shape[1]) // 2 + 1, 0)
+            ph = max((self.output_size[2] - x.shape[2]) // 2 + 1, 0)
+            pd = max((self.output_size[2] - x.shape[3]) // 2 + 1, 0)
+            x = np.pad(x, [(0, 0), (pw, pw), (ph, ph), (pd, pd)], mode='constant', constant_values=0)
         else:
             pw, ph, pd = 0, 0, 0
 
-        (w, h, d) = x.shape
-        w1 = np.random.randint(0, w - self.output_size[0])
-        h1 = np.random.randint(0, h - self.output_size[1])
+        _, w, h, d = x.shape
+        w1 = np.random.randint(0, w - self.output_size[1])
+        h1 = np.random.randint(0, h - self.output_size[2])
         d1 = np.random.randint(0, d - self.output_size[2])
 
-        def do_transform(image):
-            if image.shape[0] <= self.output_size[0] or image.shape[1] <= self.output_size[1] or image.shape[2] <= self.output_size[2]:
-                try:
-                    image = np.pad(image, [(pw, pw), (ph, ph), (pd, pd)], mode='constant', constant_values=0)
-                except Exception as e:
-                    print(e)
-            image = image[w1:w1 + self.output_size[0], h1:h1 + self.output_size[1], d1:d1 + self.output_size[2]]
-            return image
+        def do_transform(x):
+            return x[:, w1:w1 + self.output_size[1], h1:h1 + self.output_size[2], d1:d1 + self.output_size[2]]
+
         return do_transform
 
     def __call__(self, samples):
-        transform = self._get_transform(samples[0])
-        return [transform(s) for s in samples]
-
+        transform = self._get_transform(samples[0][np.newaxis, ...])  # Add batch channel
+        return [transform(samples[0]), transform(samples[1][np.newaxis, ...])[0]]
 
 
 class ToTensor(object):
-    """Convert ndarrays in sample to Tensors."""
-
     def __call__(self, sample):
-        image = sample[0]
-        image = image.reshape(1, image.shape[0], image.shape[1], image.shape[2]).astype(np.float32)
-        sample = [image] + [*sample[1:]]
-        return [torch.from_numpy(s.astype(np.float32)) for s in sample]
+        image = sample[0].astype(np.float32)  # [3, H, W, D]
+        label = sample[1].astype(np.float32)  # [H, W, D]
+        return [torch.from_numpy(image), torch.from_numpy(label)]
 
 
 if __name__ == '__main__':
     data_dir = '/content/drive/MyDrive/SemiSL/Dataset/PICAI_dataset'
     list_dir = '/content/drive/MyDrive/SemiSL/Code/PICAI_SSL/Basecode/Datasets/picai/data_split'
-    labset = PICAIDataset(data_dir, list_dir,split='lab')
-    unlabset = PICAIDataset(data_dir,list_dir,split='unlab')
-    trainset = PICAIDataset(data_dir,list_dir,split='train')
-    testset = PICAIDataset(data_dir, list_dir,split='test')
 
-    lab_sample = labset[0]
-    unlab_sample = unlabset[0]
-    train_sample = trainset[0] 
-    test_sample = testset[0]
-
-    # print(len(labset), lab_sample['image'].shape, lab_sample['label'].shape)  # 16 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-    # print(len(unlabset), unlab_sample['image'].shape, unlab_sample['label'].shape) # 64 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-    # print(len(trainset), train_sample['image'].shape, train_sample['label'].shape) # 80 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-    # print(len(testset), test_sample['image'].shape, test_sample['label'].shape) # 20 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-
-    print(len(labset), lab_sample[0].shape, lab_sample[1].shape)  # 16 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-    print(len(unlabset), unlab_sample[0].shape, unlab_sample[1].shape) # 64 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-    print(len(trainset), train_sample[0].shape, train_sample[1].shape) # 80 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-    print(len(testset), test_sample[0].shape, test_sample[1].shape) # 20 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-
-
-    # labset = LAHeart(data_dir, list_dir,split='lab', aug_times=5)
-    # unlabset = LAHeart(data_dir,list_dir,split='unlab', aug_times=5)
-    # trainset = LAHeart(data_dir,list_dir,split='train', aug_times=5)
-    # testset = LAHeart(data_dir, list_dir,split='test', aug_times=5)
-
-    labset = PICAIDataset(data_dir, list_dir, split='lab')  # No aug_times
+    labset = PICAIDataset(data_dir, list_dir, split='lab')
     unlabset = PICAIDataset(data_dir, list_dir, split='unlab')
     trainset = PICAIDataset(data_dir, list_dir, split='train')
     testset = PICAIDataset(data_dir, list_dir, split='test')
 
-
     lab_sample = labset[0]
     unlab_sample = unlabset[0]
-    train_sample = trainset[0] 
+    train_sample = trainset[0]
     test_sample = testset[0]
 
-    # print(len(labset), lab_sample['image'].shape, lab_sample['label'].shape)  # 80 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-    # print(len(unlabset), unlab_sample['image'].shape, unlab_sample['label'].shape) # 320 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-    # print(len(trainset), train_sample['image'].shape, train_sample['label'].shape) # 400 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-    # print(len(testset), test_sample['image'].shape, test_sample['label'].shape) # 20 torch.Size([1, 112, 112, 80]) torch.Size([112, 112, 80])
-
-    print(len(labset), lab_sample[0].shape, lab_sample[1].shape)  
-    print(len(unlabset), unlab_sample[0].shape, unlab_sample[1].shape) 
-    print(len(trainset), train_sample[0].shape, train_sample[1].shape)  
-    print(len(testset), test_sample[0].shape, test_sample[1].shape)  
+    print(len(labset), lab_sample[0].shape, lab_sample[1].shape)
+    print(len(unlabset), unlab_sample[0].shape, unlab_sample[1].shape)
+    print(len(trainset), train_sample[0].shape, train_sample[1].shape)
+    print(len(testset), test_sample[0].shape, test_sample[1].shape)
